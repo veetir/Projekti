7df0950185e1bc94a3de1ff38977191d7a8b2105
veetir <66322733+veetir@users.noreply.github.com>
2021-04-19 19:30:49 +0300

    Mökin lisääminen toimii. Toteutus erittäin karkea, mutta toimiva. Tietokannasta löydyttävä 3 toiminta-aluetta, jotka lisätty sinne kuten Discordissa mainittu, jotta toimii. Ohjelma ei tulosta mitään, mutta toimivuuden saa tarkistettua tietokannasta.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,11 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import java.sql.*;
+
 public class Controller {
 
     @FXML
     // Mökki
-    public TextField moknimiTextField, mokzipTextField, mokhloTextField,mokosoiteTextField, mokalueTextField, mokvarusteluTextField, mokkuvausTextField;
+    public TextField moknimiTextField, mokzipTextField, mokhloTextField, mokosoiteTextField, mokalueidTextField, mokvarusteluTextField, mokkuvausTextField;
     // Asiakas
     public TextField etunimiTextField, sukunimiTextField, zipTextField, emailTextField, puhnroTextField, osoiteTextField;
 
@@ -41,7 +43,47 @@ public class Controller {
 
     public void lisaamokkiButtonOnAction(ActionEvent actionEvent) {
         String nimi = moknimiTextField.getText();
-        System.out.println(nimi);
+        String osoite = mokosoiteTextField.getText();
+        String zip = mokzipTextField.getText();
+        String kuvaus = mokkuvausTextField.getText();
+        String hlolkm = mokhloTextField.getText();
+        String varustelu = mokvarusteluTextField.getText();
+        String alueid = mokalueidTextField.getText();
+        //System.out.println(nimi);
+        String kysely = "INSERT INTO mokki(toimintaalue_id, postinro, mokkinimi, katuosoite, kuvaus, henkilomaara, varustelu) "
+                + "VALUES(?, ?, ?, ?, ?, ?, ?);";
+
+        try {
+            Connection conn = SQL_yhteys.getYhteys();
+            PreparedStatement pstmt = conn.prepareStatement(kysely,
+                    Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, alueid);
+            pstmt.setString(2, zip);
+            pstmt.setString(3, nimi);
+            pstmt.setString(4, osoite);
+            pstmt.setString(5, kuvaus);
+            pstmt.setString(6, hlolkm);
+            pstmt.setString(7, varustelu);
+
+            int rowAffected = pstmt.executeUpdate();
+            if(rowAffected == 1)
+            {
+                // process further here
+            }
+
+            // get candidate id
+            int candidateId = 0;
+            ResultSet rs = pstmt.getGeneratedKeys();
+            if(rs.next())
+                candidateId = rs.getInt(1);
+
+
+
+        } catch (SQLException e) {
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        }
     }
 
     public void moklisaustakaisinButtonOnAction(ActionEvent actionEvent) {

4375f98d0c0f42c9e4db1f9034710f78aef0028c
veetir <66322733+veetir@users.noreply.github.com>
2021-04-23 14:50:08 +0300

    Korjattu .fxml -tiedostojen nimet vastaamaan oikeita näyttöjä. Kaikki .fxml-tiedostojen nimet alkavat nyt pienellä kirjaimella. Lisäksi muutettu nappien fx:id ja on actionien nimet vastaamaan niiden oikeita nimiä. Siirretty (nykyisestä) varaus.fxml -tiedostosta hallinta-napin toiminta hallinnan kontrolleriin. Poistettu kontrollereista OnActioneita, joiden nappeja ei ole enää olemassa.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("hallinta.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("varaus.fxml"));
         primaryStage.setTitle("Projekti");
         primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
--- src/sample/RaportitController.java
+++ src/sample/RaportitController.java
@@ -6,22 +6,28 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 
 public class RaportitController {
-    public void raportistaKotiinButtonOnAction(ActionEvent actionEvent) throws IOException {
-        Parent toiseenNakymaan = FXMLLoader.load(getClass().getResource("kotinakyma.fxml"));
+
+    public void varausButtonOnAction(ActionEvent actionEvent) throws IOException {
+        Parent toiseenNakymaan = FXMLLoader.load(getClass().getResource("varaus.fxml"));
         Scene toinenScene = new Scene(toiseenNakymaan);
 
-        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         window.setScene(toinenScene);
         window.show();
+
     }
 
-    public void raporttiAikaButton(ActionEvent actionEvent) {
-    }
+    public void hallintaButtonOnAction(ActionEvent actionEvent) throws IOException {
+        Parent toiseenNakymaan = FXMLLoader.load(getClass().getResource("hallinta.fxml"));
+        Scene toinenScene = new Scene(toiseenNakymaan);
 
-    public void raporttiToimiAlueButtonOnAction(ActionEvent actionEvent) {
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setScene(toinenScene);
+        window.show();
     }
 }

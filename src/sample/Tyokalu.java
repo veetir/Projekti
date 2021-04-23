62dac7a2d82c1a0fe3f14ee902e16a7aa7d8467d
veetir <66322733+veetir@users.noreply.github.com>
2021-04-23 17:05:23 +0300

    Luokka Tyokalu, jossa metodi vaihdaIkkuna.

---
+++ src/sample/Tyokalu.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Tyokalu {
+
+    static void vaihdaIkkuna(String fxml, ActionEvent ikkuna) throws IOException {
+        Parent toiseenNakymaan = FXMLLoader.load(Tyokalu.class.getResource(fxml));
+        Scene toinenScene = new Scene(toiseenNakymaan);
+
+        Stage window = (Stage) ((Node) ikkuna.getSource()).getScene().getWindow();
+        window.setScene(toinenScene);
+        window.show();
+    }
+
+}

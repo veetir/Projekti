269228c84e3badd6d2515cc2d10be64865ff2c68
Japi1984 <82207379+Japi1984@users.noreply.github.com>
2021-04-25 15:59:50 +0300

    Raportit välilehden ulkonäkö v 1.0

--- src/sample/RaportitController.java
+++ src/sample/RaportitController.java
@@ -2,15 +2,36 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.fxml.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 
 public class RaportitController {
+
+
 
     public void varausButtonOnAction(ActionEvent actionEvent) throws IOException {
         Parent toiseenNakymaan = FXMLLoader.load(getClass().getResource("varaus.fxml"));
